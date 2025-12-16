b2a93e216004b9458f966c436aca111225041f66
Sergey Lushkin <serg.nn1985@gmail.com>
2025-12-16 23:02:17 +0300

    Добавил lesson1 typescript

---
+++ ts/src/primitives.ts
@@ -0,0 +1,29 @@
+// 1. string
+
+let name: string = 'Sergey'
+name = 'Ivan'
+
+//2. number
+let age = 35;
+age = 36
+
+//3. boolean
+
+let isAdmin: boolean = true
+
+//4. null
+
+const counter = null
+
+// 5. undefined
+
+const response = undefined
+
+// 6. symbol
+
+const symb: symbol = Symbol('1')
+
+// 7. bigint
+
+const bigData: bigint = 123123123123n
+
