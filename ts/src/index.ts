d090cc8300b499bd7334ba877f11dbe4a22ab101
Sergey Lushkin <serg.nn1985@gmail.com>
2025-12-23 22:53:49 +0300

    Добавил lesson2

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,14 +1,25 @@
-import { price } from "./union"
+import { price } from "./union";
 
-console.log('Hello, typescript!')
-console.log('Hello, javascript!')
+console.log("Hello, typescript!");
+console.log("Hello, javascript!");
 
 console.log(price);
 
-const h2 = document.createElement('h2');
-h2.innerText = 'Subtitle';
-document.body.append(h2);
+if (typeof document !== "undefined") {
+    const h2 = document.createElement("h2");
+    h2.innerText = "Subtitle";
+    document.body.append(h2);
 
-h2.addEventListener('click', (event) => {
-    console.log(event.target)
-})
+    h2.addEventListener("click", (event) => {
+        console.log(event.target);
+    });
+}
+
+enum Books {
+    JS = "JS Book",
+    TS = "TS Book",
+    HTML = "HTML Book",
+}
+
+const htmlBook = Books.HTML;
+console.log("htmlBook", htmlBook);
