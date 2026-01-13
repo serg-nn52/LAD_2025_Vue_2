a1958df8fd859d0fb03d8b68f93d7351f5bc157f
Sergey Lushkin <serg.nn1985@gmail.com>
2026-01-13 21:45:47 +0300

    Добавил lesson3

---
+++ vue_2025_2/src/components/ClickCounter.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="counter-wrapper">
+    <div class="counter">
+      <h2 :style="counterTitleColor">{{ title }}</h2>
+      <div>Count: {{ count }}</div>
+      <div>Double count: {{ doubleCount }}</div>
+    </div>
+    <button @click="count++">Increment++</button>
+    <button @click="decrement">Decrement--</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Default counter title',
+    },
+  },
+  emits: ['decrement'],
+  mounted() {
+    console.log(this.$attrs)
+  },
+  computed: {
+    doubleCount() {
+      return 2 * this.count
+    },
+    counterTitleColor() {
+      return { color: this.count > 5 ? 'red' : 'black' }
+    },
+  },
+  methods: {
+    showDecrement() {
+      console.log('decrement')
+    },
+    decrement() {
+      // this.showDecrement()
+      this.count--
+      this.$emit('decrement', this.count)
+    },
+    reset() {
+      this.count = 0
+    },
+  },
+  watch: {
+    count: {
+      handler(newValue, oldValue) {
+        console.log('newValue', newValue)
+        console.log('oldValue', oldValue)
+        // if (newValue === 0) alert('Значение равно 0!')
+      },
+      immediate: true,
+      deep: true,
+    },
+  },
+})
+</script>
+
+<style scoped>
+button {
+  padding: 5px 10px;
+  border: none;
+  background-color: aqua;
+  border-radius: 5px;
+  margin-right: 5px;
+  cursor: pointer;
+}
+</style>
