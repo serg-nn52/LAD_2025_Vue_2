f9fae0238014e8e9c6a477198ed2657b8659e236
Sergey Lushkin <serg.nn1985@gmail.com>
2026-01-29 23:42:51 +0300

    Добавляем сборку для деплоя

---
+++ ts/dist/enum.js
@@ -0,0 +1,8 @@
+var Books;
+(function (Books) {
+    Books["JS"] = "JS Book";
+    Books["TS"] = "TS Book";
+    Books["HTML"] = "HTML Book";
+})(Books || (Books = {}));
+const htmlBook = Books.HTML;
+export {};
---
+++ ts/dist/union.js
@@ -0,0 +1,8 @@
+//union
+let id = 1;
+id = '2';
+export let price = null;
+let discountPrice = 55;
+price = '30';
+let theme = 'light';
+theme = "green";
