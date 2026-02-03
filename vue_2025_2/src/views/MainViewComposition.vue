1144c298c194dd300a8e23ac0c5c0cc02b126887
Sergey Lushkin <serg.nn1985@gmail.com>
2026-02-03 22:17:12 +0300

    Добавил lesson6

--- vue_2025_2/src/views/MainViewComposition.vue
+++ vue_2025_2/src/views/MainViewComposition.vue
@@ -1,5 +1,5 @@
 <template>
-  <MainLayout>
+  <MainLayout class="main-layout">
     <ClickCounterComposition
       @decrement="
         (event) => {
@@ -7,12 +7,63 @@
         }
       "
     />
+    <section :class="styles.users">
+      <h2>Пользователи</h2>
+      <div :class="styles['users-block']">
+        <UserCardComposition
+          :user="{
+            name: 'Sergey',
+            age: 40,
+            role: 'Teacher',
+            img: 'https://img.freepik.com/premium-photo/picture-boy-with-hoodie-that-says-hes_644690-179027.jpg',
+          }"
+        >
+          <template #append>
+            <p>*Признан иноагентом</p>
+          </template>
+        </UserCardComposition>
+        <UserCardComposition
+          :user="{
+            name: 'Ilya',
+            age: 30,
+            role: 'Student',
+            img: 'https://img.freepik.com/premium-photo/romantic-cartoonlike-3d-pixar-character-zhao-with-glasses_899449-139436.jpg',
+          }"
+        >
+          <template #prepend>
+            <h2>Title prepend</h2>
+          </template>
+        </UserCardComposition>
+      </div>
+    </section>
+    <!-- <button :class="styles.button">Test button</button> -->
   </MainLayout>
 </template>
 
 <script setup lang="ts">
 import ClickCounterComposition from '@/components/composition/ClickCounterComposition.vue';
+import UserCardComposition from '@/components/composition/UserCardComposition/UserCardComosition.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
+import { useCssModule } from 'vue';
+
+const styles = useCssModule(); //this.$styles
 </script>
 
-<style scoped></style>
+<style lang="scss" module>
+.button {
+  display: block;
+  background-color: green;
+}
+.title {
+  color: red !important;
+}
+.counter {
+  background-color: red;
+}
+.users-block {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-top: 15px;
+}
+</style>
